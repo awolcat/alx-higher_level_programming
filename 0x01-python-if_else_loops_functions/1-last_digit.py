99eabc0f6d75c75c3ec9ee569bf77e81c0de70db
Albert <albertirura11@gmail.com>
2023-05-09 14:13:27 -0700

    two programs and two functions

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,8 +7,8 @@ if number < 0:
     if last_digit == 0:
         print(f"Last digit of {-number} is {last_digit} and is 0")
     else:
-        print(f"Last digit of {-number} is {-last_digit}\
-                and is less than 6 and not 0")
+        print(f"Last digit of {-number} is {-last_digit} \
+and is less than 6 and not 0")
 else:
     last_digit = number % 10
     if last_digit > 5:
@@ -16,5 +16,5 @@ else:
     elif last_digit == 0:
         print(f"Last digit of {number} is {last_digit} and is 0")
     else:
-        print(f"Last digit of {number} is {last_digit}\
-                and is less than 6 and not 0")
+        print(f"Last digit of {number} is {last_digit} \
+and is less than 6 and not 0")
---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+def uppercase(str):
+    new_str = ""
+    for a in str:
+        if ord(a) >= 97 and ord(a) <= 122:
+            a = chr(ord(a) - 32)
+        new_str = new_str + a
+    print(new_str)
