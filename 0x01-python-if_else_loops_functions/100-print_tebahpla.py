355d65867cbb7a530f5ee35667c260dcfb91ce0a
Albert <albertirura11@gmail.com>
2023-05-10 14:31:03 -0700

    print alphabet in reverse and alternate case

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+i = 122
+j = 122
+while j >= 97:
+    if j % 2 != 0:
+        i =  chr(j - 32)
+        j = j - 1
+    else:
+        i = chr(j)
+        j = j - 1
+    print("{}".format(i), end="")
