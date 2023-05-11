89af5a89b5b49bcd893e01a2187ab5422e7efdd5
Albert <albertirura11@gmail.com>
2023-05-11 09:51:01 -0700

    script takes arguments from terminal, counts and prints them

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    args = len(argv)
+    count = 1
+    if args == 1:
+        print("0 arguments.")
+    elif args == 2:
+        print("1 argument:")
+        print(1, argv[1])
+    else:
+        print("{} arguments:".format(args - 1))
+        for i in argv:
+            if i != argv[0]:
+                print("{} {}".format(count, i))
+                count = count + 1
