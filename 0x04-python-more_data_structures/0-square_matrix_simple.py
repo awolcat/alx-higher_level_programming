ce735636dae81d4030b22b3a5e71a96b741e027b
Albert <albertirura11@gmail.com>
2023-05-17 09:20:41 -0700

    list comprehension to find the square of elements in a 2d matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    [new_matrix.append(row) for row in matrix]
+    new_matrix = [[ x ** 2 for x in row] for row in new_matrix]
+    return new_matrix
