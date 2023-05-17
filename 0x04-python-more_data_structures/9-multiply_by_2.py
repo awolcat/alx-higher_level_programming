f8d21ecd641797ed48f1b4459821349160759739
Albert <albertirura11@gmail.com>
2023-05-17 11:02:16 -0700

    pycodstyle fix

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,5 +2,5 @@
 def multiply_by_2(a_dictionary):
     new_dict = a_dictionary.copy()
     for key in a_dictionary:
-        new_dict[key] =  a_dictionary[key] * 2
+        new_dict[key] = a_dictionary[key] * 2
     return new_dict
