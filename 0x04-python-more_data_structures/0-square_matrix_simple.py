2e188f6000a9def67061db40c3612b95b06f037c
Albert <albertirura11@gmail.com>
2023-05-17 09:23:22 -0700

    pycodestyle fix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,5 +2,5 @@
 def square_matrix_simple(matrix=[]):
     new_matrix = []
     [new_matrix.append(row) for row in matrix]
-    new_matrix = [[ x ** 2 for x in row] for row in new_matrix]
+    new_matrix = [[x ** 2 for x in row] for row in new_matrix]
     return new_matrix
