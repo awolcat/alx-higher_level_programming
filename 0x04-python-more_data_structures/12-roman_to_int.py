0c5ea38e63b32b3dbea949dcb4fa6ccf1890dcde
Albert <albertirura11@gmail.com>
2023-05-17 17:24:20 -0700

    function converts roman numeral to integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,19 +2,22 @@
 def roman_to_int(roman_string):
     number = 0
     roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    if roman_string != "" and roman_string != None:
-        if len(roman_string) == 1:
-            number = roman[roman_string[0]]
-            return number
-        if roman[roman_string[0]] < roman[roman_string[-1]]:
-            number = roman[roman_string[-1]]
-            for i in range(len(roman_string) - 1):
-                number -= roman[roman_string[i]]
-            return number
-        elif roman[roman_string[0]] > roman[roman_string[-1]]:
-            number = roman[roman_string[0]]
-            for i in range(1, len(roman_string)):
-                number += roman[roman_string[i]]
-            return number
+    if isinstance(roman_string, str):
+        if roman_string is not None and len(roman_string) > 0:
+            if len(roman_string) == 1:
+                number = roman[roman_string[0]]
+                return number
+            elif roman[roman_string[0]] < roman[roman_string[-1]]:
+                number = roman[roman_string[-1]]
+                for i in range(len(roman_string) - 1):
+                    number -= roman[roman_string[i]]
+                return number
+            elif roman[roman_string[0]] > roman[roman_string[-1]]:
+                number = roman[roman_string[0]]
+                for i in range(1, len(roman_string)):
+                    number += roman[roman_string[i]]
+                return number
+        else:
+            return 0
     else:
         return 0
