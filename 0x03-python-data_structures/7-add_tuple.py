19ec32e6c766b22ed2e40f269f61a08b0259ae0f
Albert <albertirura11@gmail.com>
2023-05-19 04:49:14 -0700

    first element of a tuple can only be empty if the whole tuple is empty

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    ans = 0
-    ans2 = 0
-    x, y, x1, y1 = 0, 0, 0, 0
-    x, y = tuple_a
-    x1, y1 = tuple_b
-    ans = x + x1
-    ans2 = y + y1
-    my_tuple = (ans, ans2)
-    return my_tuple
+    x1 = tuple_a[0] if len(tuple_a) > 0 else 0
+    y1 = tuple_a[1] if len(tuple_a) > 1 else 0
+    x2 = tuple_b[0] if len(tuple_b) > 0 else 0
+    y2 = tuple_b[1] if len(tuple_b) > 1 else 0
+    sum1 = x1 + x2
+    sum2 = y1 + y2
+    return (sum1, sum2)
