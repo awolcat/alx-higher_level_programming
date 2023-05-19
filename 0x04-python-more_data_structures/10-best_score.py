3a49a0e9ad8e357ea56907231f958587e4034118
Albert <albertirura11@gmail.com>
2023-05-19 14:40:46 -0700

    use len to find empty sequences

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     number = 0
     student_list = []
-    if a_dictionary is not None:
+    if len(a_dictionary) > 0:
         for key in a_dictionary:
             if a_dictionary[key] > number:
                 number = a_dictionary[key]
