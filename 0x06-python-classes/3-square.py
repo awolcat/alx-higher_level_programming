767ab8281f2fb8d16595dc6acb89bdd50c6e76e5
Albert <albertirura11@gmail.com>
2023-05-30 12:26:45 -0700

    task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module for my first Python Class"""
+
+
+class Square:
+    """My first Python Class"""
+
+    def __init__(self, size=0):
+        """Initialize size"""
+        if isinstance(size, int) is False:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Return the square"""
+        return self.__size ** 2
