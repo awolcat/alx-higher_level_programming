a7847088644dd008d434487f0adca3776bb4fddc
Albert <albertirura11@gmail.com>
2023-05-30 11:20:32 -0700

    initialize a private attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module for my first Python Class"""
+
+
+class Square:
+    """My first Python Class"""
+
+    def __init__(self, size):
+        """Initialize size"""
+        self.__size = size
