40ff660816c510b8bcbf19c03cf4990c4d30c0a4
Albert <albertirura11@gmail.com>
2023-06-05 07:48:37 -0700

    task 0

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    This module contains the definition Class: Rectangle
+
+    The class is defined below
+"""
+class Rectangle:
+    """
+        This classs defines a rectangle
+
+        The class also defines many functionalities
+        that can be applied to any Rectangle object
+    """
+    pass
