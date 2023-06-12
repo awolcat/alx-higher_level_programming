0124e8b4a338577560f428837c599aff3edeb2ba
Albert <albertirura11@gmail.com>
2023-06-12 11:39:26 -0700

    task 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    This module contains definition of a single function
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+        This function checks if an object obj
+        is of the class a_class
+    """
+    return type(obj) is a_class
