b88d84a71cc402743b94e8284d13a02bab1fe988
Albert <albertirura11@gmail.com>
2023-06-12 14:01:24 -0700

    added str magic method and area method

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+    This module defines a new class: Rectangle
+"""
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+        This Rectangle class is a subclass of BaseGeometry
+    """
+    def __init__(self, width, height):
+        """
+            Initialize width and height attributes
+            for the object
+        """
+        BaseGeometry.integer_validator(self, "width", width)
+        self.__width = width
+        BaseGeometry.integer_validator(self, "height", height)
+        self.__height = height
+
+    def area(self):
+        """
+            This method calculates and returns the area of
+            a rectangle objects
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """
+            This magic method defines the print format
+            for Rectangle objects
+        """
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
