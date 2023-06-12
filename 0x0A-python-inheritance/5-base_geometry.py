5cb738cc8f5711f78c0cf2856596a6c1be6032ae
Albert <albertirura11@gmail.com>
2023-06-12 11:56:16 -0700

    task 5: empty class

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+    This module defines a new class: BaseGeometry
+"""
+
+
+class BaseGeometry():
+    """
+        This is a base class that defines different objects
+    """
+    pass
