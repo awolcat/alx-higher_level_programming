94b57c8dafdcf3c283b0349369dba2c17f145d44
Albert <albertirura11@gmail.com>
2023-06-13 08:14:11 -0700

    added a fuction to serialize python objects to JSON strings

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-import json
 """
     This module defines one function: to_json_string
 """
 
 
 def to_json_string(my_obj):
+    import json
     """
         This function will accept any object type
         and attempt to serialize it to a JSON
