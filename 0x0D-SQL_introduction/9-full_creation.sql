9e5df9c098d7f183f5f23dc131f76c71ade31e56
Albert <albertirura11@gmail.com>
2023-07-18 11:50:11 -0700

    MySQL Intro

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,9 @@
+-- Create a table
+-- Only create table if it does not exist. db name in cmd line
+CREATE TABLE IF NOT EXISTS second_table (
+	id INT,
+	name VARCHAR(256),
+	score INT
+	);
+INSERT INTO second_table (id,name,score)
+VALUES(1,'John',10), (2,'Alex',3), (3,'Bob',14), (4,'George',8);
