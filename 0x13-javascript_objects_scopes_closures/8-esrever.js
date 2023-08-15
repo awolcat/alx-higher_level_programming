009fdbeed95ce5bf49de5bd8dde4f5b9b3ea0155
Albert <albertirura11@gmail.com>
2023-08-15 21:51:24 +0300

    8 Reverse an Array

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,9 @@
+exports.esrever = function (list) {
+  const newArr = [];
+  let m = 0;
+  for (let n = list.length - 1; n > -1; n--) {
+    newArr[m] = list[n];
+    m++;
+  }
+  return newArr;
+};
