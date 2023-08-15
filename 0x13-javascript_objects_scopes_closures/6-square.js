27ac4c228aecc07b4b6a32409712d2ce3549dc8c
Albert <albertirura11@gmail.com>
2023-08-15 20:50:53 +0300

    6 Square Print Method

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,18 +1,18 @@
 #!/usr/bin/node
 
-const Rectangle = require('./4-rectangle.js');
+const ogSquare = require('./5-square.js');
 
-class Square extends Rectangle {
+class Square extends ogSquare {
   constructor (size) {
-    super(size, size);
+    super(size);
   }
 
   charPrint (c) {
     if (c === undefined) {
       this.print();
     } else {
-        for (let n = 0; n < this.height; n++) {
-          for (let m = 0; m < this.height; m++) {
+        for (let n = 0; n < this.size; n++) {
+          for (let m = 0; m < this.size; m++) {
             process.stdout.write(c);
           }
           console.log();
