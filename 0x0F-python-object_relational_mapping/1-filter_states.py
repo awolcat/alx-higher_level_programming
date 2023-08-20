5964e0187c5e5c953eee4eaa25a751f7e69279fb
Albert <albertirura11@gmail.com>
2023-08-20 15:39:43 +0300

    MySQLdb Filter

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""This module lists all states whose name starts with capital N
+    in the database specified.
+"""
+import sys
+import MySQLdb
+
+
+if __name__ == '__main__':
+    args = sys.argv
+    User = args[1]
+    Pass = args[2]
+    Database = args[3]
+    db = MySQLdb.connect(host="localhost", user=User, password=Pass,
+                         database=Database)
+    cursor = db.cursor()
+    cursor.execute("""SELECT * FROM states
+                   WHERE states.name LIKE 'N%'
+                   ORDER BY states.id ASC""")
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
