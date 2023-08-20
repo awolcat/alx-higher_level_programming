7027bca8b4bcdbe7143bceed9f20a698a1b4f43a
Albert <albertirura11@gmail.com>
2023-08-20 16:43:59 +0300

    MySQLdb Filter by CommandLine state name

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""This script will connect to the named database
+    and query state names passed as CommandLine arguments
+"""
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    args = sys.argv
+    Host = 'localhost'
+    User = args[1]
+    Pass = args[2]
+    dbName = args[3]
+    name = args[4]
+
+    query = """SELECT * FROM states
+            WHERE name = '{}'
+            ORDER BY id ASC""".format(name)
+    db = MySQLdb.connect(host=Host, user=User, password=Pass, database=dbName)
+    cursor = db.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    for result in results:
+        print(result)
