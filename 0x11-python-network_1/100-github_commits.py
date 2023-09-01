940f2f95bb1dda6476184dddfe11e616e0380c62
Albert <albertirura11@gmail.com>
2023-09-01 19:13:59 +0300

    github API commits

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -12,7 +12,9 @@ repo = sys.argv[2]
 URL = f'https://api.github.com/repos/{owner}/{repo}/commits'
 
 response = requests.get(URL)
-for commit in response.json():
+
+for index in range(0, 10):
+    commit = response.json()[index]
     sha = commit.get('sha')
     author = commit.get('commit').get('author').get('name')
     print('{}: {}'.format(sha, author))
