515c41b9f9294afa2c2c6f07c8d6f8ba8dba460b
Albert <albertirura11@gmail.com>
2023-09-01 18:47:05 +0300

    GitHub API

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""This module defines a script that takes your 
-    GitHub credentials (username and password) 
+"""This module defines a script that takes your
+    GitHub credentials (username and password)
     and uses the GitHub API to display your id
 """
 import sys
