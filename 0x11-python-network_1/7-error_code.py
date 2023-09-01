4d3baea6894dca88c798cec569b9475f49225d8a
Albert <albertirura11@gmail.com>
2023-09-01 17:42:17 +0300

    requests package error code

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -8,8 +8,10 @@ import sys
 import requests
 
 if __name__ == '__main__':
-    response = requests.get(sys.argv[1])
-    if response.status_code < 400:
+    try:
+        response = requests.get(sys.argv[1])
+        err = response.raise_for_status()
         print('{}'.format(response.text))
-    else:
-        print('Error code: {}'.format(response.status_code))
+    except Exception:
+        if response.status_code >= 400:
+            print('Error code: {}'.format(response.status_code))
