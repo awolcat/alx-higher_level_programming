d19893da95efe58c97bb17ae0499322f2017b097
Albert <albertirura11@gmail.com>
2023-09-01 17:32:16 +0300

    requests package error code

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This module defines a script that attempts to fetch
+    a URL and prints the response body or the error code
+    if a http or server error (>=400)
+    Usage: 7-error_code.py <url>
+"""
+import sys
+import requests
+
+if __name__ == '__main__':
+    response = requests.get(sys.argv[1])
+    if response.status_code < 400:
+        print(response.text)
+    else:
+        print('Error code: {}'.format(response.status_code))
